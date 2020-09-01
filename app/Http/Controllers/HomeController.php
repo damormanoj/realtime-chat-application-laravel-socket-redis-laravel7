a95dda57623484931bb362bb211f73437691e1e5
Damor Manoj <damormanoj12345@gmail.com>
2020-09-01 16:57:57 +0530

    add new demo project
    add  demo project

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        $users = User::where('id', '!=', Auth::id())->get();
+        $this->data['users'] = $users;
+
+        return view('home',$this->data);
+    }
+}
